3f375238266fa6bf4640523c48cf58583f9f49a9
SantiagoProgramador <andresx1277@gmail.com>
2024-06-04 06:18:23 -0500

    First commit

---
+++ src/main/java/com/riwi/filtro/FiltroApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.filtro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/filtro/FiltroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.filtro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
