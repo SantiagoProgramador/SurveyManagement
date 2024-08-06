9c2f070c5dd4465b50fd8f759faecd240311dfef
SantiagoProgramador <andresx1277@gmail.com>
2024-08-05 23:05:54 -0500

    Working on Issue bug #9

--- src/main/java/com/riwi/filtro/infrastructure/abstracts/IUserService.java
+++ src/main/java/com/riwi/filtro/infrastructure/abstracts/IUserService.java
@@ -1,13 +1,11 @@
 package com.riwi.filtro.infrastructure.abstracts;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-
 import com.riwi.filtro.api.dto.request.UserRequest;
 import com.riwi.filtro.api.dto.request.update.UserUpdateRequest;
 import com.riwi.filtro.api.dto.response.UserResponse;
 import com.riwi.filtro.domain.entities.User;
 
 public interface IUserService
-    extends BaseService<User, UserRequest, UserResponse, Long, UserUpdateRequest>, UserDetailsService {
+        extends BaseService<User, UserRequest, UserResponse, Long, UserUpdateRequest> {
 
 }
--- src/main/java/com/riwi/filtro/infrastructure/services/UserService.java
+++ src/main/java/com/riwi/filtro/infrastructure/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,7 +34,7 @@ import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
-public class UserService implements IUserService {
+public class UserService implements IUserService, UserDetailsService {
 
   @Autowired
   private final UserRepository userRepository;
